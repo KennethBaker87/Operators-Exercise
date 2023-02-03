6877a5e69dc6cc0d8d1e77d819929e6fb71e121e
Kenneth Baker <spyksaturn@gmail.com>
2023-02-02 19:45:18 -0500

    initial

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace OperatorExercise
+﻿using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int diff = a - b;
+            int quot = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+
+            //--------------------------Exercise 2-------------------------------
+            Console.WriteLine("What is radius of your circle?: ");
+            var radius = double.Parse(Console.ReadLine());
+            var answer = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with raidus of {radius} is {answer}");
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            return (Math.PI * radius * radius);
         }
     }
 }
